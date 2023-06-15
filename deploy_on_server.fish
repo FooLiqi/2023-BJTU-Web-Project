c600746b4f9c13ad212aa567ded742e2756c0ad8
YXHXianYu <2943003@qq.com>
2023-06-16 01:18:08 +0800

    feat: 部署了前端

--- deploy_on_server.fish
+++ deploy_on_server.fish
@@ -1,3 +1,4 @@
+# Backend
 conda activate smbackend
 cd back-end
 cd ScarletMana
@@ -6,3 +7,8 @@ python manage.py migrate
 nohup python manage.py runserver 0.0.0.0:8000 --noreload > sm.log 2>&1 &
 cd ..
 cd ..
+
+# Frontend
+# cd front-end
+# sudo yarn start &
+# cd ..
